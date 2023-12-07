34f129d50fa16115259b53c4d110ca9fac2c1112
ansunhong <ash8663@gmail.com>
2023-12-07 23:41:01 +0900

    topbar and something

---
+++ assets/js/srollbar.js
@@ -0,0 +1,9 @@
+const scoll_bar = document.querySelector('#scoll_bar');
+        
+window.addEventListener("scroll", () => {
+    let fullHeight = document.body.scrollHeight - window.innerHeight;
+    let scroll = pageYOffset;
+    let progress = (scroll/fullHeight)*100;
+
+    scoll_bar.style.width = progress + '%';
+})
