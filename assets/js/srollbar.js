bf361f2e3a50766faace2ed8ef97ca4842d9b9e9
ansunhong <ash8663@gmail.com>
2023-12-09 19:39:22 +0900

    fixed bug

--- assets/js/srollbar.js
+++ assets/js/srollbar.js
@@ -2,8 +2,9 @@ const scoll_bar = document.querySelector('#scoll_bar');
         
 window.addEventListener("scroll", () => {
     let fullHeight = document.body.scrollHeight - window.innerHeight;
-    let scroll = pageYOffset;
+    let scroll = scrollY;
     let progress = (scroll/fullHeight)*100;
 
     scoll_bar.style.width = progress + '%';
 })
+
