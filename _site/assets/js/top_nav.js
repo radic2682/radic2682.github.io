ff4185b212bfb49e358f6ad527e5bc911fd72d73
An Sun Hong <ash8663@naver.com>
2023-12-13 17:23:14 +0900

    search func

--- _site/assets/js/top_nav.js
+++ _site/assets/js/top_nav.js
@@ -4,7 +4,6 @@ const domain = window.location.origin.endsWith('/') ? window.location.origin : w
 
 const isRootPage = currentUrl === domain;
 const isVisitingPage = currentUrl.includes('/visiting');
-const isSearchPage = currentUrl.includes('/search');
 
 
 const logo = document.querySelector('.logo');
@@ -12,13 +11,13 @@ const top_search = document.querySelector('#top_search');
 const backg = document.querySelector('.backg');
 
 
-if(!isRootPage && !isVisitingPage && !isSearchPage) {
+if(!isRootPage && !isVisitingPage) {
     logo.src = "/assets/images/logoB_y.png";
     top_search.src = "/assets/images/icon_search_l_y.png";
 }
 
 window.addEventListener("scroll", () => {
-    if(!isRootPage && !isVisitingPage && !isSearchPage) {
+    if(!isRootPage && !isVisitingPage) {
         if(window.scrollY < backg.offsetHeight - 40 ) {
             // console.log(window.scrollY);
             logo.src = "/assets/images/logoB_y.png";
